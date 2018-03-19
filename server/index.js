9b8ed8f50ec166b0c959d9e3d5b1feada41731c4
Alex Levine <alexlevine@Alexs-MacBook-Pro-2.local>
2018-03-19 19:03:06 -0400

    signup form renders and queries postgres

--- server/index.js
+++ server/index.js
@@ -17,17 +17,8 @@ app.use(express.static(__dirname + '/../dist'))
 app.use(bodyParser.json())
 
 
-// app.post('/newAccount', function(req, res){
-//   let email = req.body.newAccount.email
-//   let password = req.body.newAccount.password
-//   let firstName = req.body.newAccount.firstName
-//   let lastName = req .body.newAccount.lastName  
-//   let userClass = req.body.newAccount.userClass
-  
-//   console.log('server/index.js - new account -', email, password, firstName, lastName, userClass)  
-// })
-
 // Sign up
+
   app.post('/newAccount', (req, res)=> {
     const {firstName, lastName, email, password, userClass} = req.body.newAccount
 
