9012647816216d8e54f2939d47824b1c4cdfcd67
Alex Levine <alexlevine@Alexs-MacBook-Pro-2.local>
2018-03-19 17:26:16 -0400

    rendering with redux installed

--- server/index.js
+++ server/index.js
@@ -17,7 +17,6 @@ app.use(express.static(__dirname + '/../dist'))
 app.use(bodyParser.json())
 
 
-<<<<<<< HEAD
 // app.post('/newAccount', function(req, res){
 //   let email = req.body.newAccount.email
 //   let password = req.body.newAccount.password
@@ -27,17 +26,6 @@ app.use(bodyParser.json())
   
 //   console.log('server/index.js - new account -', email, password, firstName, lastName, userClass)  
 // })
-=======
-app.post('/newAccount', function(req, res){
-  let email = req.body.newAccount.email
-  let password = req.body.newAccount.password
-  let firstName = req.body.newAccount.firstName
-  let lastName = req .body.newAccount.lastName  
-  let userClass = req.body.newAccount.userClass
-  
-  console.log('server/index.js - new account -', email, password, firstName, lastName, userClass)  
-})
->>>>>>> title is now userClass
 
 // Sign up
   app.post('/newAccount', (req, res)=> {
