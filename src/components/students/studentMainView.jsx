b37197a14dde523abbf928a77eedef6ef9bedee1
Alex Levine <alexlevine@Alexs-MacBook-Pro-2.local>
2018-03-26 19:25:03 -0400

    student clicks on enter class and is taken directly to quiz if it is active

--- src/components/students/studentMainView.jsx
+++ src/components/students/studentMainView.jsx
@@ -71,6 +71,7 @@ class StudentMainView extends React.Component {
                 <Button
                   label="Enter Class"
                   type="button"
+                  // path="/studentQuiz"
                   path="/studentLiveClass"
                   // path="default"
                   primary={true}
