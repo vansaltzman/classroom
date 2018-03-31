4ad9aebd46be2f18e595fb2e79e92233b47348d2
Alex Levine <alexlevine@Alexs-MacBook-Pro-2.local>
2018-03-31 10:53:09 -0400

    thumbPoll component file

---
+++ src/components/teachers/thumbPoll.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import axios from "axios"
+import {connect} from 'react-redux';
+import { bindActionCreators } from "redux";
+import * as actions from '../../actions/index.js';
+
+
+class ThumbPoll extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      test: ''
+    }
+  }
+
+
+
+
+  render() {
+
+    return (
+      <div>
+      
+        
+      </div>
+    )
+  }
+}
+export default ThumbPoll;
